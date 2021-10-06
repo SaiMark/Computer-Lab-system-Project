b63ff872082bb2c971d43cd76da2e82a075dd621
SaiMark <59070894+SaiMark@users.noreply.github.com>
2021-10-07 03:44:06 +1030

    Global file created
    using #progma once to create a global file. Adding some text file for storage information. The purpose of text file is  Input /output

---
+++ globalFile.h
@@ -0,0 +1,12 @@
+#pragma once
+
+//Admin file
+#define ADMIN_FILE		"admin.txt"
+//Student file
+#define STUDENT_H       "student.txt"
+//Teacher file
+#define TECHER_FILE     "teacher.txt"
+//Computer Lab File
+#define COMPUTER_LAB    "computerRoom.txt"
+//Order file
+#define ORDER_FILE      "order.txt"
